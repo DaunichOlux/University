754b573098bc42a68fc3d42d391f90cbb65e2f0c
DaunichOlux <nikita.rogovik2006@yandex.ru>
2024-12-16 20:08:37 +0500

    Laboratory work №7
    16.12.24

---
+++ First Semester/лаборатрная работа №7/Метод Ньютона/Метод Ньютона/Метод Ньютона.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+// кол - во итераций
+int k = 0;
+const double eps = 1e-6;
+
+// Функция
+double F(double x) {
+    return 0.25 * pow(x,3) + x - 1.2502;
+}
+// Производная функции
+double derivF(double x) {
+    return (0.75 * pow(x, 2)) + 1;
+}
+// Вторая производная функции
+double derivF2(double x) {
+    return 1.5 * x;
+}
+// Функция для метода Ньютона
+double NewtonMetod(double x) {
+    double h = F(x) / derivF(x);
+    while (abs(h) >= eps)
+    {
+        ++k;
+        h = F(x) / derivF(x);
+        x = x - h;
+    }
+    return x;
+}
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double a, b;
+    double x = 0;
+    cout << "Введите левую границу интервала (a): " << endl;
+    cin >> a;
+    cout << "Введите правую границу интервала (b): " << endl;
+    cin >> b;
+
+    if (F(a) * derivF2(a) > 0)
+        x = a;
+    else
+        x = b;
+    cout << NewtonMetod(x) << endl << "Количество итераций: " << k;
+
+    return 0;
+}
---
+++ First Semester/лаборатрная работа №7/Метод половинного деления/Метод половинного деления/Метод половинного деления/Метод половинного деления.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+//epsilon
+const double eps = 1e-6;
+
+//кол-во итераций
+int k;
+
+//Функция
+double F(double x) {
+    return 0.25 * pow(x, 3) + x - 1.2502;
+}
+
+//Функция метода половинного деления
+double Fhalf(double a, double b) {
+    double x, Fb, Fx;
+    while (abs(b - a) >= eps)
+    {
+        x = (a + b) / 2;
+        cout << x << endl;
+        k++;
+
+        Fb = F(b);
+        Fx = F(x);
+
+        if (Fb * Fx < 0)
+            a = x;
+        else
+            b = x;
+    }
+    return x;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    double a, b;
+    cout << "Введите левую границу(a): " << endl;
+    cin >> a;
+    cout << "Введите правую границу(b): " << endl;
+    cin >> b;
+
+    cout << "Корень: " << Fhalf(a,b) << endl << "Итераций: " << k << endl;
+    return 0;
+}
+
