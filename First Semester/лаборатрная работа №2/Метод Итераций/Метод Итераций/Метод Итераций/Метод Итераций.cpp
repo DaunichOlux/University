03ea8adfb8a399e3a96f60aab61e0d1bb0fc41e6
DaunichOlux <nikita.rogovik2006@yandex.ru>
2024-12-20 10:58:15 +0500

    Add files via upload

---
+++ First Semester/лаборатрная работа №2/Метод Итераций/Метод Итераций/Метод Итераций/Метод Итераций.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+// epsilon
+const double eps = 1e-6;
+
+//кол-во итераций
+int k = 0;
+
+//Функция
+double F(double x) {
+    x = -0.25 * pow(x, 3) + 1.2502;
+    return x;
+}
+
+//Функция метода итераций
+double iter(double x, double eps) {
+    double x0;
+    do {
+        x0 = x;
+        x = F(x0);
+        k++;
+        cout << x << endl;
+    } while (abs(x0 - x) >= eps);
+    return x;
+}
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double a, b;
+    
+    cout << "Введите левуя границу(a): ";
+    cin >> a;
+    cout << "Введите правую границу(b): ";
+    cin >> b;
+    double x0 = (a+b) / 2;
+
+    double x = iter(x0, eps);
+
+    cout << "Корень: " << x << endl << "Итераций: " << k;
+    return 0;
+}
+
